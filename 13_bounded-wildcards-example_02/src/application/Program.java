fc700e804c0ab301f68368bf4310334bfe1c2050
Cindy Okino <cindy.okino@gmail.com>
2020-04-15 09:36:20 -0400

    Add 13_bounded-wildcards-example_02

---
+++ 13_bounded-wildcards-example_02/src/application/Program.java
@@ -0,0 +1,35 @@
+// COVARIANCIA E CONTRAVARIANCIA
+package application;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+	
+	public static void main(String[] args) {
+
+	List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+	List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+	List<Object> myObjs = new ArrayList<Object>();
+	
+	copy(myInts, myObjs);
+	printList(myObjs);
+	copy(myDoubles, myObjs);
+	printList(myObjs);
+	
+	}
+
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) { // lista de origem é covariancia, posso acessar os elementos
+			destiny.add(number); // lista de destino é contravariancia, posso adicionar elementos
+		}		
+	}	
+	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
+	}
+}

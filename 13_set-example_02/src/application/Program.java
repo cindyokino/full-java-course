65fcbf019c7081a721f88406d25f61e6f6beed78
Cindy Okino <cindy.okino@gmail.com>
2020-04-16 08:40:45 -0400

    Add how TreeSet compares elements and update previous files

--- 13_set-example_02/src/application/Program.java
+++ 13_set-example_02/src/application/Program.java
@@ -5,7 +5,9 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Program {
+	
 	public static void main(String[] args) {
+		
 		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
 		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
 //union

827eeb3a2192a48ffa1caed26ed113bfd3c9e03e
Cindy Okino <cindy.okino@gmail.com>
2020-04-19 16:01:24 -0400

    Anonymous class

--- 15_comparator-example_01/src/application/Program.java
+++ 15_comparator-example_01/src/application/Program.java
@@ -1,8 +1,9 @@
-//Standard Comparator - Comparator objeto de classe separada
+//Comparator objeto de classe anonima
 
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -12,12 +13,19 @@ public class Program {
 	public static void main(String[] args) {
 
 		List<Product> list = new ArrayList<>();
-
+		
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-
-		list.sort(new MyComparator());
+		
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+		
+		list.sort(comp);
 
 		for (Product p : list) {
 			System.out.println(p);
--- 15_comparator-example_01/src/entities/Product.java
+++ 15_comparator-example_01/src/entities/Product.java
@@ -4,7 +4,7 @@ public class Product {
 
 	private String name;
 	private Double price;
-
+	
 	public Product() {
 	}
 

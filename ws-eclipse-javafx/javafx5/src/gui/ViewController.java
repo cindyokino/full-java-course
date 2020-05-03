a92d7e916e47bf6d0980ecb5cb81b55f208d2e43
Cindy Okino <cindy.okino@gmail.com>
2020-05-03 14:51:01 -0400

    Add javafx5 - Adding constraints fot the TextFields, using Initializable interface

---
+++ ws-eclipse-javafx/javafx5/src/gui/ViewController.java
@@ -0,0 +1,50 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Alerts;
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController implements Initializable { 
+//interface Initializable é usada para colocar algum codigo para ser executado na inicializacao do controlador 
+	
+	@FXML
+	private TextField txtNumber1;
+		
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
+	
+	@FXML
+	public void onBtSumAction() {
+		try {			
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			double sum = number1 + number2;
+			labelResult.setText(String.format("%.2f", sum));
+		}
+		catch (NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) { 
+		Constraints.setTextFieldDouble(txtNumber1); //essa funcao deixa usar doubles
+		Constraints.setTextFieldDouble(txtNumber2);
+		Constraints.setTextFieldMaxLength(txtNumber1, 12); //uso a funcao que criamos em Constraints para limitar o numero de digitos
+		Constraints.setTextFieldMaxLength(txtNumber2, 12);
+	}
+}

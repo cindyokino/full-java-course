621a0d1ca0206b20499549bbceb958ceb4ecaf51
Cindy Okino <cindy.okino@gmail.com>
2020-05-11 21:11:52 -0400

    jpa class maven - testing persist and remove

--- jpaclassmaven/src/main/java/aplication/Program.java
+++ jpaclassmaven/src/main/java/aplication/Program.java
@@ -9,20 +9,32 @@ import domain.Person;
 public class Program {
 	
 	public static void main(String[] args) {
-		
-		Person p1 = new Person(null, "Carlos Silva", "carlos@gmail.com"); //id é null, pois o banco de dados vai gerar um
-		Person p2 = new Person(null, "Ana Torres", "ana@gmail.com");
-		Person p3 = new Person(null, "Maria Flores", "maria@gmail.com");
+		//PARA POPULAR A BASE DE DADOS
+//		Person p1 = new Person(null, "Carlos Silva", "carlos@gmail.com"); //id é null, pois o banco de dados vai gerar um
+//		Person p2 = new Person(null, "Ana Torres", "ana@gmail.com");
+//		Person p3 = new Person(null, "Maria Flores", "maria@gmail.com");
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemple-jpa"); //instanciar EntityManagerFactory para criar EntityManager 
 		EntityManager em = emf.createEntityManager(); //automaticamente obtem conexao com banco de dados e persistencia de dados implementada
 		
-		em.getTransaction().begin(); //para iniciar transacao com o banco de dados
-		em.persist(p1); //pega o objeto (p1) e salva no banco de dados
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit(); //para confirmar as transacoes que eu fiz
+//		em.getTransaction().begin(); //para iniciar transacao com o banco de dados
+//		em.persist(p1); //pega o objeto (p1) e salva no banco de dados
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit(); //para confirmar as transacoes que eu fiz
 	
+		
+		//PARA BUSCAR NO BANCO DE DADOS
+		Person p = em.find(Person.class, 2); //buscar pessoa com id=2 no banco de dados
+		System.out.println(p);
+			
+		//PARA DELETAR O OBJETO BUSCADO ACIMA
+//		em.getTransaction().begin();
+//		em .remove(p);
+//		em.getTransaction().commit();
+		
 		System.out.println("Ready!");
+		em.close();
+		emf.close();
 	}
 }
